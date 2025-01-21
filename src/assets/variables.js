37d958d08b23bd0664983425a9fafc6e8de79b0c
farzin abbasi <frzin.af@gmail.com>
2025-01-21 16:28:34 +0330

    few more steps to go

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,4 +1,4 @@
 const KEY2 = "a2cc76f6";
 const KEY = "cf775363";
-export const url = `http://www.omdbapi.com/?apikey=${KEY2}&`;
-export const urlImg = `http://img.omdbapi.com/?apikey=${KEY}&`;
+export const url = `https://www.omdbapi.com/?apikey=${KEY2}&`;
+export const urlImg = `https://img.omdbapi.com/?apikey=${KEY}&`;
